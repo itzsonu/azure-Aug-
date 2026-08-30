761d6f7c4cf17e5e6284b0478400f86b88e11942
itzsonu <sk1593020@gmail.com>
2026-08-30 06:45:17 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "terraform_demo" {
+  name     = "TerraformDemoRG"
+  location = "Central India"
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+    description = "Name of the Azure Resource Group"
+    type = string
+}
+
+variable "location" {
+    description = "Azure region"
+    type = string 
+
+}
+
+variable "storage_acocunt_name" {
+    description = "Globally unique Azure Storage Account name"
+    type = string 
+}
